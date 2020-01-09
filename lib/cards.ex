01e41e54fc6f7eed4ccc88f4d1662a78840c1a63
Pablo Manzano <pablo.manzano@civica-soft.com>
2020-01-09 10:49:07 +0100

    [ADD] - Learn operator pipe, pattern matching, using module of the own library Elixir

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,61 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
 
-  ## Examples
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
 
-      iex> Cards.hello()
-      :world
+    # List.flatten(cards)
+  end
 
-  """
-  def hello do
-    :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  # La interrogación da un significado semántico al método,
+  # diciendo que devolverá un valor booleano
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    # { hand: [], deck: []}
+    # Pattern Matching  // Destructuring JS
+    # {hand, rest_of_deck } = Enum.split(deck, hand_size)
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+
+    # Con guión bajo, le decimos al compilador, que estamos cumpliendo Pattern Matching pero que nos da igual ese valor
+    # el compilador no nos dará la advertencia de variable no usada
+  end
+
+  def create_hand(hand_size) do
+    # deck = Cards.create_deck()
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, hand_size)
+    # es lo mismo que ... , esto se puede hacer teniendo
+    # parámetros consistentes entre los métodos encadenados
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
