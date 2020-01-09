da0c526648b9bce3517eb3e02f2415d0e198b55e
Pablo Manzano <pablo.manzano@civica-soft.com>
2020-01-09 11:50:27 +0100

    [ADD] - Learn doctest and test and documentation module and functions

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Providers methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Return a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -16,16 +23,50 @@ defmodule Cards do
     # List.flatten(cards)
   end
 
+  @doc """
+    Shuffle the deck.
+    The argument `deck` is the list of cards
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card
+    Then `deck` argument is the list of cards and `card` is the card we want to find in the list.
+
+    ## Examples
+
+        iex> deck = Cards.create_deck
+        ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+        "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+        "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+        "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+        "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
+  """
+  def contains?(deck, card) do
   # La interrogación da un significado semántico al método,
   # diciendo que devolverá un valor booleano
-  def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divide a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+    The argument `deck` is the list of cards.
+
+    ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> { hand, _deck } = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     # { hand: [], deck: []}
     # Pattern Matching  // Destructuring JS
@@ -33,11 +74,18 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Save file in filesystem.
+    The argument `deck` is the list of cards.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Laod file from filesystem
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -48,6 +96,11 @@ defmodule Cards do
     # el compilador no nos dará la advertencia de variable no usada
   end
 
+  @doc """
+    Create hand joinning create deck, shuffle and deal.
+    Then `hand_size` argument indicates how many cards should
+    be in the hand.
+  """
   def create_hand(hand_size) do
     # deck = Cards.create_deck()
     # deck = Cards.shuffle(deck)
